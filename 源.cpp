5de6a2987294e9be604a3b5404e987633be86848
fushw <2548602105@qq.com>
2020-08-17 20:59:51 +0800

    D:/desktop/Document/C++/c++code/c++finalExam/源.cpp

---
+++ 源.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <algorithm> 
+using namespace std;
+/*
+void reverse(const string& a);
+
+int main()
+{
+    string str;
+    cout << " Please enter a string " << endl;
+    getline(cin, str);
+    reverse(str);
+    return 0;
+}
+void reverse(const string& a)
+{
+    string s = a;
+    int len = a.size();
+    for (int i = len - 1; i >= 0; i--)
+    {
+        s[len - i - 1] = a[i];
+    }
+    s[len] = '\0';
+    cout << s << endl;
+}
+*/
+
+/*
+#include "Complex.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    complex left(1, 2), right(3, 4);
+
+    complex result = left + right;
+    complex result1 = left - right;
+    complex result2 = left * right;
+    complex result3 = left / right;
+    cout << result<<endl;
+    cout << result1<<endl;
+    cout << result2 << endl;
+    cout << result3 << endl;
+    cin >> left;
+    cout << left << endl;
+
+    return 0;
+}
+*/
+
